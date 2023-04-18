cde3f8e6d329d9a8eba62295b45d05a2c4a78c4f
Rui Afonso <afonso.r.dev@gmail.com>
2023-04-18 13:30:26 +0200

    create user registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserRegistrationFormComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
