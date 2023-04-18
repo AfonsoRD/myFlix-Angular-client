8627f6c99082e752e1325c7d50d4d5fddc1521cf
Rui Afonso <afonso.r.dev@gmail.com>
2023-04-18 19:32:26 +0200

    update api url

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://movie-api-drab.vercel.app/';
+const apiUrl = 'https://movie-api-drab.vercel.app';
 @Injectable({
   providedIn: 'root',
 })
